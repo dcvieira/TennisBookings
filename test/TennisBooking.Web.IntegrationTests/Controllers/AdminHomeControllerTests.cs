4fd02ae4657122d2674bbb33295c87c405ad3a34
dcvieira <dcvieira@tjsp.jus.br>
2022-03-21 18:55:40 -0300

    Add Auth tests

--- test/TennisBooking.Web.IntegrationTests/Controllers/AdminHomeControllerTests.cs
+++ test/TennisBooking.Web.IntegrationTests/Controllers/AdminHomeControllerTests.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Net;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using TennisBookings.Web.IntegrationTests.Helpers;
 using Xunit;
 
 namespace TennisBookings.Web.IntegrationTests.Controllers
@@ -15,5 +20,44 @@ namespace TennisBookings.Web.IntegrationTests.Controllers
             factory.ClientOptions.AllowAutoRedirect = false;            
             _factory = factory;
         }
+
+        [Fact]
+        public async Task Get_SecurePageIsForbiddenForAnUnauthenticatedUser()
+        {
+            var client =_factory.CreateClient();
+
+            var response = await client.GetAsync("/Admin");
+
+            Assert.Equal(HttpStatusCode.Redirect, response.StatusCode);
+            Assert.StartsWith("http://localhost/identity/account/login", response.Headers.Location.OriginalString, StringComparison.OrdinalIgnoreCase);
+
+        }
+
+        public static IEnumerable<object[]> RoleAccess => new List<object[]>
+        {
+            new object[] { "Member", HttpStatusCode.Forbidden},
+            new object[] { "Admin", HttpStatusCode.OK }
+        };
+
+        [Theory]
+        [MemberData(nameof(RoleAccess))]
+        public async Task Get_SecurePageAccessibleOnlyByAdminUsers(string role, HttpStatusCode expected)
+        {
+            var client = _factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureTestServices(services =>
+                {
+                    services.AddAuthentication("Test").AddScheme<TestAuthenticationSchemeOptions, TestAuthenticationHandler>("Test",
+                        options => options.Role = role);
+                });
+            }).CreateClient();
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Test");
+
+            var response = await client.GetAsync("/Admin");
+
+            Assert.Equal(expected, response.StatusCode);
+        }
+
     }
 }
--- test/TennisBooking.Web.IntegrationTests/Pages/HomePageTests.cs
+++ test/TennisBooking.Web.IntegrationTests/Pages/HomePageTests.cs
@@ -1,4 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AngleSharp.Dom;
+using Microsoft.Extensions.Configuration;
+using TennisBookings.Web.IntegrationTests.Helpers;
 using Xunit;
 
 namespace TennisBookings.Web.IntegrationTests.Pages
@@ -10,6 +15,58 @@ namespace TennisBookings.Web.IntegrationTests.Pages
         public HomePageTests(CustomWebApplicationFactory<Startup> factory)
         {
             _factory = factory;
-        }        
+        }
+
+        [Fact]
+        public async Task Get_RetunsPageWithExpectedH1()
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync("/");
+
+            response.EnsureSuccessStatusCode();
+
+            using var content = await HtmlHelpers.GetDocumentAsync(response);
+
+            var h1 = content.QuerySelector("h1");
+
+            Assert.Equal("Welcome to Tennis by the Sea!", h1.TextContent);
+        }
+
+        public static IEnumerable<object[]> ConfigVarations => new List<object[]>
+        {
+            new object[] { false, false, false},
+            new object[] { true, false, false},
+            new object[] { false, true, false},
+            new object[] { true, true, true},
+        };
+
+        [Theory]
+        [MemberData(nameof(ConfigVarations))]
+        public async Task HomePageIncludesForecast_ForExpectedConfigurations(bool globalEnabled, bool pageEnabled, bool shouldShow)
+        {
+            var client = _factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureAppConfiguration((ctx, config) =>
+                {
+                    config.AddInMemoryCollection(new Dictionary<string, string>
+                    {
+                        {"Features:WeatherForecasting:EnableWeatherForecast", globalEnabled.ToString() },
+                        {"Features:HomePage:EnableWeatherForecast", pageEnabled.ToString() },
+                    });
+                });
+            }).CreateClient();
+
+            var response = await client.GetAsync("/");
+
+            using var content = await HtmlHelpers.GetDocumentAsync(response);
+
+            var forecastDiv = content.All.SingleOrDefault(e => e.Id == "weather-forecast" && e.LocalName == TagNames.Div);
+
+            Assert.Equal(shouldShow, forecastDiv != null);
+
+        }
+
+
     }
 }
